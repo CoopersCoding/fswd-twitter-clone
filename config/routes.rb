07022daf8103c98a408583ca13c6ca294840ae46
Brian Cooper <codewithcoop@gmail.com>
2026-08-30 18:48:03 -0400

    Fix Active Storage image routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,20 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   namespace :api do
-    # USERS
     post '/users'                  => 'users#create'
 
-    # SESSIONS
     post '/sessions'               => 'sessions#create'
-    get  '/authenticated'          => 'sessions#authenticated'
+    get '/authenticated'           => 'sessions#authenticated'
     delete '/sessions'             => 'sessions#destroy'
 
-    # TWEETS
     post '/tweets'                 => 'tweets#create'
-    get  '/tweets'                 => 'tweets#index'
+    get '/tweets'                  => 'tweets#index'
     delete '/tweets/:id'           => 'tweets#destroy'
-    get  '/users/:username/tweets' => 'tweets#index_by_user'
-    get  '/tweets/search/:keyword' => 'tweets#search'
+
+    get '/users/:username/tweets'  => 'tweets#index_by_user'
+    get '/tweets/search/:keyword'  => 'tweets#search'
   end
-  
-  get '*path' => 'static_pages#home'
-  # if you are using active storage to upload and store images, comment the above line
+
+  # React user profile route
+  get '/:username' => 'static_pages#home'
 end
