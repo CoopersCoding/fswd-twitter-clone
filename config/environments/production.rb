18f33ac2068ecaf59d6104a9fea1bb905ee50422
Brian Cooper <144545253+CoopersCoding@users.noreply.github.com>
2026-09-02 16:03:48 -0400

    Restore local Active Storage configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,12 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
 
-  # Persist uploads outside Heroku's ephemeral dyno filesystem.
-  config.active_storage.service = :amazon
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -108,7 +112,8 @@ Rails.application.configure do
   #
   # By default Rails will store a last write timestamp in the session. The
   # DatabaseSelector middleware is designed as such you can define your own
-  # strategy for connection switching and pass that into the middleware through these options.
+  # strategy for connection switching and pass that into the middleware through
+  # these configuration options.
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
